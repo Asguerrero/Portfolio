74d94cf9225065d0d8af601f6c866d12bfb07f76
Asguerrero <valentinaguerrero.as@hotmail.com>
2020-08-10 11:59:37 -0500

    Test for mobile devices

--- draw.js
+++ draw.js
@@ -2,6 +2,8 @@
 
 let shouldPaint = true;
 
+
+
 canvas = document.getElementById('gameCanvas')
 context = canvas.getContext('2d')
 
@@ -11,9 +13,19 @@ canvas.width = window.innerWidth * 0.98;
 //canvas.addEventListener("mousedown", mouseDown);
 //canvas.addEventListener("mouseup", mouseUp);
 //canvas.addEventListener("mousemove", mouseMove);
-canvas.addEventListener("touchmove", mouseMove);
+canvas.addEventListener("touchstart", mouseMove);
+//canvas.addEventListener("touchmove", mouseMove);
 //canvas.addEventListener("touchend", mouseUp);
 
+
+function update() {
+
+
+	canvas.height = window.innerHeight * 0.15;
+canvas.width = window.innerWidth * 0.98;
+		
+	
+}
 
 function mouseDown (event) {
 	shouldPaint = true;
@@ -36,10 +48,10 @@ function mouseMove (){
 
 function paint(){
 	context.fillStyle = "#2D749F";
-
   var rect = event.target.getBoundingClientRect();
   var x = event.clientX - rect.left; //x position within the element.
   var y = event.clientY - rect.top;  //y position within the element.
 	context.fillRect(x, y, 15, 15);
 }
+
 
