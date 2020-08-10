b6fabbf1cfbddd9b9a406f8168119df4103f0a6b
Asguerrero <valentinaguerrero.as@hotmail.com>
2020-08-10 11:33:20 -0500

    Ajusted canvas for mobile devices

--- draw.js
+++ draw.js
@@ -1,26 +1,29 @@
 // initialize config variables here
 
-let shouldPaint = false;
+let shouldPaint = true;
 
 canvas = document.getElementById('gameCanvas')
 context = canvas.getContext('2d')
 
-canvas.height = window.innerHeight * 0.25;
+canvas.height = window.innerHeight * 0.15;
 canvas.width = window.innerWidth * 0.98;
 
 canvas.addEventListener("mousedown", mouseDown);
 canvas.addEventListener("mouseup", mouseUp);
 canvas.addEventListener("mousemove", mouseMove);
+object.addEventListener("touchmove", mouseMove);
+object.addEventListener("touchend", mouseUp);
 
 
 function mouseDown (event) {
 	shouldPaint = true;
+	context.clearRect(0,0, canvas.width, canvas.height);
 	
 }
 
 function mouseUp (){
 	
-	shouldPaint = false;
+	shouldPaint = true;
 	context.clearRect(0,0, canvas.width, canvas.height);
 }
 
