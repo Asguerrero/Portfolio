14aa93225381cbf7974912f9b6e081cca2962155
Asguerrero <valentinaguerrero.as@hotmail.com>
2020-08-10 12:08:04 -0500

    Test for mobile devices

--- draw.js
+++ draw.js
@@ -2,7 +2,10 @@
 
 let shouldPaint = true;
 
+let initialWidth = window.innerWidth;
+console.log(window.innerWidth)
 
+let initialHeight = window.innerHeight;
 
 canvas = document.getElementById('gameCanvas')
 context = canvas.getContext('2d')
@@ -10,33 +13,33 @@ context = canvas.getContext('2d')
 canvas.height = window.innerHeight * 0.15;
 canvas.width = window.innerWidth * 0.98;
 
-//canvas.addEventListener("mousedown", mouseDown);
-//canvas.addEventListener("mouseup", mouseUp);
-//canvas.addEventListener("mousemove", mouseMove);
-canvas.addEventListener("touchstart", mouseMove);
-//canvas.addEventListener("touchmove", mouseMove);
-//canvas.addEventListener("touchend", mouseUp);
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("mouseup", mouseUp);
+canvas.addEventListener("mousemove", mouseMove);
+canvas.addEventListener("click", mouseMove);
+
 
 
 function update() {
 
-
-	canvas.height = window.innerHeight * 0.15;
-canvas.width = window.innerWidth * 0.98;
+	if (window.innerWidth!= initialWidth)  {
+		canvas.width = window.innerWidth * 0.98;
+		console.log(window.innerWidth)
 		
+		}
 	
 }
 
 function mouseDown (event) {
 	shouldPaint = true;
-	context.clearRect(0,0, canvas.width, canvas.height);
+	//context.clearRect(0,0, canvas.width, canvas.height);
 	
 }
 
 function mouseUp (){
 	
 	shouldPaint = true;
-	context.clearRect(0,0, canvas.width, canvas.height);
+	//context.clearRect(0,0, canvas.width, canvas.height);
 }
 
 function mouseMove (){
@@ -55,3 +58,4 @@ function paint(){
 }
 
 
+update();
